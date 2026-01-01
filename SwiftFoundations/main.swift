0fd1bb12b8db87850f30aefa31d6a19af85bdc3f
Gopinath Chandrasekaran <gopinath.chand@gmail.com>
2026-01-01 10:57:42 -0800

    first commit

--- SwiftFoundations/main.swift
+++ SwiftFoundations/main.swift
@@ -7,5 +7,46 @@
 
 import Foundation
 
-print("Hello, World!")
-
+struct FilePath: Hashable, CustomStringConvertible {
+    let path: String
+    // confirm to customStringConvertible, get
+    var description: String { path }
+    
+    init?(path: String) {
+        guard path.hasPrefix("/") else { return nil }
+        guard !path.utf8.contains(0) else { return nil }
+        
+        self.path = FilePath.normalized(path)
+    }
+    
+    var url: URL { URL(fileURLWithPath: self.path, isDirectory: self.path.hasSuffix("/")) }
+    
+    func appending(component: String) -> FilePath? {
+        guard !component.isEmpty else { return nil }
+        guard !component.hasPrefix("/") else { return nil }
+        guard !component.utf8.contains(0) else { return nil }
+        
+        let newUrl = self.url.appendingPathComponent(component, isDirectory: false)
+        return FilePath(path: newUrl.path)
+    }
+    
+    static func normalized(_ path: String) -> String {
+        var normalizedPath = ""
+        var prevWasSlash: Bool = false
+        
+        for ch in path {
+            if ch == "/" {
+                if !prevWasSlash {
+                    normalizedPath.append(ch)
+                }
+                prevWasSlash = true
+            }
+            else {
+                prevWasSlash = false
+                normalizedPath.append(ch)
+            }
+        }
+        
+        return normalizedPath
+    }
+}
